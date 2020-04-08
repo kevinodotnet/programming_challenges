b77bb5ef3f5052459c95ccb8c629644ec4b83e42
Kevin O'Donnell <kevin.odonnell@shopify.com>
2020-04-08 12:41:06 -0400

    lesson 4

---
+++ 004_make_it_so/lesson.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+#
+# This program, when run, should print out the following:
+#
+# Hello, my name is Robot. What is your name? 
+# > 
+#
+# Nice to meet you NAME, have a nice day!
+
+puts "Farty fart fart fart."
+print ">>>>>>>>>>>>>>>>"
+name = $stdin.readline.delete("\n")
+puts ""
+puts "Farty McFart Fart #{name} blah #{name} blah #{name} blah, have a poop day!"
+
